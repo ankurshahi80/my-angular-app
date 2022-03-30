09f3897fdcae8a9672a7254beae3734258fe65f9
Ankur Shahi <shahiankur80@gmail.com>
2022-03-30 11:19:54 -0400

    two way data-binding example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { TwoWayDatabindingDemoComponent } from './two-way-databinding-demo/two-way-databinding-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    TwoWayDatabindingDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/two-way-databinding-demo/two-way-databinding-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-databinding-demo',
+  templateUrl: './two-way-databinding-demo.component.html',
+  styleUrls: ['./two-way-databinding-demo.component.css']
+})
+export class TwoWayDatabindingDemoComponent implements OnInit {
+  output!: String;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
